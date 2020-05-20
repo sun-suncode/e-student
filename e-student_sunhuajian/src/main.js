847ca09a1563e638c121aa010be00ed0dfa25f12
sunhuajian <sunhuajian1214@gmail.com>
2020-05-20 16:34:55 +0800

    e-student_sunhuajian

--- e-student_sunhuajian/src/main.js
+++ e-student_sunhuajian/src/main.js
@@ -4,6 +4,10 @@ import './assets/css/reset.css'
 import 'font-awesome/css/font-awesome.min.css' 
 import router from "./router"
 import Router from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
 Vue.use(Router)
 
 Vue.config.productionTip = false
